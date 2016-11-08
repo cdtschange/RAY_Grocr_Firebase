13b18ddd0e22284152225f1e9fea51553f11c8e7
wei.mao <wei.mao@jimubox.com>
2016-11-08 14:17:51 +0800

    add data

--- Grocr/Grocr/GroceryListTableViewController.swift
+++ Grocr/Grocr/GroceryListTableViewController.swift
@@ -24,6 +24,9 @@ import UIKit
 
 class GroceryListTableViewController: UITableViewController {
 
+  let ref = FIRDatabase.database().reference(withPath: "grocery-items")
+
+  
   // MARK: Constants
   let listToUsers = "ListToUsers"
   
@@ -109,12 +112,24 @@ class GroceryListTableViewController: UITableViewController {
     
     let saveAction = UIAlertAction(title: "Save",
                                    style: .default) { action in
-      let textField = alert.textFields![0] 
-      let groceryItem = GroceryItem(name: textField.text!,
-                                    addedByUser: self.user.email,
-                                    completed: false)
-      self.items.append(groceryItem)
-      self.tableView.reloadData()
+//      let textField = alert.textFields![0] 
+//      let groceryItem = GroceryItem(name: textField.text!,
+//                                    addedByUser: self.user.email,
+//                                    completed: false)
+//      self.items.append(groceryItem)
+//      self.tableView.reloadData()
+        guard let textField = alert.textFields?.first,
+          let text = textField.text else { return }
+        
+        // 2
+        let groceryItem = GroceryItem(name: text,
+                                      addedByUser: self.user.email,
+                                      completed: false)
+        // 3
+        let groceryItemRef = self.ref.child(text.lowercased())
+        
+        // 4
+        groceryItemRef.setValue(groceryItem.toAnyObject())
     }
     
     let cancelAction = UIAlertAction(title: "Cancel",
